20ddcf64aab8e3203467481b6bd5117efadc2c93
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-12-28 12:08:02 +0545

    day-1

--- Day-2.js
+++ Day-2.js
@@ -53,3 +53,42 @@ let asking = (question, yes, no) =>{
 }
 
 asking("Do you agree", showYes, showNO)
+
+// objects 
+
+function user(name, age){
+    return{
+        name : name,
+        age :  age
+    }
+}
+
+let makeUser = user("rubin", 23)
+
+console.log(makeUser.name)
+
+// check whether object has following property or not
+
+console.log("name" in makeUser) // true if there is name property
+
+// itterable objects
+
+for (let key in makeUser){
+    console.log(key)
+    console.log(makeUser[key])
+}
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+function multyplyNumeric(obj) {
+    for (key in obj){
+        if (typeof(obj[key])=='number'){
+            obj[key] *= 2;
+        };
+    }
+}
+multyplyNumeric(menu)
+console.log(menu)
