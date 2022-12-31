741dc7490a343580c32f8b32ba02fb20997742ed
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-12-31 12:36:11 +0545

    day-3

--- Day-3.js
+++ Day-3.js
@@ -2,9 +2,28 @@
 
 // Rounding 
 
-console.log(Math.floor(3.54)) // 3
+    // console.log(Math.floor(3.54)) // 3
 
-console.log(Math.ceil(3.2)) // 4
+    // console.log(Math.ceil(3.2)) // 4
 
-console.log(3.122222.toFixed(2)) // 3.12
+    // console.log(3.122222.toFixed(2)) // 3.12
 
+    // // other method
+
+    // console.log(Math.max(11,12,13,14,55))
+    // console.log(Math.min(20,24,55,01))
+    // console.log(Math.floor(Math.random()*100))
+    // console.log(Math.floor(Math.random()*7))
+
+// String
+
+    /* Backticks, however, allow us to embed any expression 
+    into the string, by wrapping it in ${…} */
+
+    // function sum(a,b){
+    //     return a + b
+    // }
+
+    // console.log(`1 + 2 = ${sum(1,2)}`) // 1 + 2 = 3
+
+    
