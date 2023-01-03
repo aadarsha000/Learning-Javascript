baff66f9de00f9b94ecc71bbd6fb00ff0781dc37
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2023-01-03 21:19:32 +0545

    day-4

--- Day-4.js
+++ Day-4.js
@@ -1,1 +1,31 @@
-// Date and Time
+// Date
+
+    // creation 
+        let now = new Date()
+        console.log(now)
+
+    // Access date compnents
+        console.log(now.getFullYear()) // gets the year
+
+        console.log(now.getMonth()) // gets months
+
+        console.log(now.getDay())
+
+        console.log(now.getDate())
+
+// json
+
+    // let student = {
+    //     name: 'John',
+    //     age: 30,
+    //     isAdmin: false,
+    //     courses: ['html', 'css', 'js'],
+    //     spouse: null
+    //   };
+    
+    //   let json = JSON.stringify(student);
+    
+    //   alert(typeof json); // we've got a string!
+    
+    //   alert(json);
+
